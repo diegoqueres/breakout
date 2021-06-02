7d0f0a4963fb6b0601e872768577f1b7b2ec1d8e
Diego Queres <diego.queres@stoom.com.br>
2021-06-02 12:01:29 -0300

    Lesson 1: Crazy balls'

--- core/src/net/diegoqueres/breakout/Breakout.java
+++ core/src/net/diegoqueres/breakout/Breakout.java
@@ -1,31 +1,41 @@
 package net.diegoqueres.breakout;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Breakout extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	ShapeRenderer shape;
+	List<Ball> balls = new ArrayList<>();
+	Random r = new Random();
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shape = new ShapeRenderer();
+		for (int i = 0; i < 10; i++) {
+			balls.add(new Ball(r.nextInt(Gdx.graphics.getWidth()), r.nextInt(Gdx.graphics.getHeight()),
+					r.nextInt(100),
+					r.nextInt(15), r.nextInt(15))
+			);
+		}
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		shape.begin(ShapeRenderer.ShapeType.Filled);
+		for (Ball ball : balls) {
+			ball.update();
+			ball.draw(shape);
+		}
+		shape.end();
 	}
 }
